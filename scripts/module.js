42b0bc3235b90649c16f13630d8ec082d5bd982c
TrooperMaXX <troopermaxx@ymail.com>
2024-03-31 23:32:15 +0200

    v0.5

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,34 @@
-Hooks.once('init', async function() {
+import registerSettings from "./settings.js";
 
+
+
+Hooks.once('init', async function() {
+    console.log('hexxen-tools | init');
+    registerSettings();
 });
 
 Hooks.once('ready', async function() {
+    console.log('hexxen-tools | Lasst die Scheuenen BRENNEN!!');
+});
 
+Hooks.on("combatStart", async function (combat) {
+    console.log('hexxen-tools | Combat Started');
+    if (game.user.isGM && game.settings.get('hexxen-tools', 'auto-roll-ini')) {
+        combat.rollAll();
+    }
+    if (game.user.isGM && game.settings.get('hexxen-tools', 'add-ini-0')) {
+        const combatants = combat.setupTurns();
+        console.log(`hexxen-tools | Kämpferys ${combatants}`);
+        if (!combatants.find((element) => element.name === 'Ini 0')){
+            const combatant = await game.combat.createEmbeddedDocuments("Combatant", [{
+                name: 'Ini 0',
+                img: game.settings.get('hexxen-tools', 'ini-0-img'),
+                hidden: false, 
+                initiative: 0
+            }]);
+        }else{
+            console.log('hexxen-tools | Hat schon Ini 0');
+        }
+    }
+   
 });
---
+++ scripts/settings.js
@@ -0,0 +1,30 @@
+export default function registerSettings() {
+  game.settings.register('hexxen-tools', 'auto-roll-ini', {
+    name: "Automatisch Initiative würfeln",
+    hint: "Soll zu Kampfbeginn die Initiative für alle automatisch gewürfelt werden? ",
+    type: Boolean,
+    default: true,
+    scope: 'world',
+    config: true,
+  });
+
+  game.settings.register('hexxen-tools', 'add-ini-0', {
+    name: "Automatisch Ini 0 hinzufügen",
+    hint: "Soll zu Kampfbeginn ein 'Ini 0' Eintrag hinzugefügt werden? ",
+    type: Boolean,
+    default: true,
+    scope: 'world',
+    config: true,
+  });
+  
+  game.settings.register('hexxen-tools', 'ini-0-img', {
+    name: "Bild für Ini 0",
+    hint: "Welches Bild soll für 'Ini 0' angezeigt werden?",
+    type: String,
+    default: 'modules/hexxen-tools/img/Ini0_1.webp',
+    filePicker: true,
+    scope: 'world',
+    config: true,
+  });
+
+}
